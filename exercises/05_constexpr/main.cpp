30266ae160bf0a0f5e6452e7523dce046b8dcab9
Simon-CHOU <mrsimonzhou@gmail.com>
2025-02-01 22:10:37 +0800

    learning-cxx: 03 04 05 completed

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -18,8 +18,10 @@ int main(int argc, char **argv) {
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
+    constexpr auto ANS_N = 20;//90;  “正确的改法是吧递归深度改小””
     constexpr auto ANS = fibonacci(ANS_N);
+    // auto ANS = fibonacci(ANS_N); // 去掉 constexpr 会避免编译期计算报错，但运行时会因为N太深，算不出来
+
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
