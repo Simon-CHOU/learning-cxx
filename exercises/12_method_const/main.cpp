06b0ad3d9e8cbf174f425f4b4f6b9ca95a000b84
Simon-CHOU <mrsimonzhou@gmail.com>
2025-03-14 16:34:05 +0800

    11 Method Const

--- exercises/12_method_const/main.cpp
+++ exercises/12_method_const/main.cpp
@@ -5,12 +5,13 @@
 struct Fibonacci {
     int numbers[11];
     // TODO: 修改方法签名和实现，使测试通过
-    int get(int i) {
+    int get(int i) const { // 添加 const 限定符
+        return numbers[i]; // 这表示 get 方法不会修改 Fibonacci 对象的成员变量，因此可以在 constexpr 实例上调用
     }
 };
 
 int main(int argc, char **argv) {
-    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
+    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};//constexpr 对象是编译时常量，这意味着它们的值在编译时就确定了，并且在运行时不能被修改。
     ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
     return 0;
