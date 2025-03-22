e1cfbcc600b2b4522c3b7d7f8ee00fd6eb9495e0
Simon-CHOU <mrsimonzhou@gmail.com>
2025-03-22 17:59:13 +0800

    函数模板

--- exercises/20_function_template/main.cpp
+++ exercises/20_function_template/main.cpp
@@ -2,9 +2,13 @@
 
 // READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
 // TODO: 将这个函数模板化
-int plus(int a, int b) {
+template<class T>
+T plus(T a, T b) {
     return a + b;
-}
+} // 二进制膨胀、增量编译失效
+// int plus(int a, int b) {
+//     return a + b;
+// }
 
 int main(int argc, char **argv) {
     ASSERT(plus(1, 2) == 3, "Plus two int");
@@ -14,7 +18,8 @@ int main(int argc, char **argv) {
     ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
     ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
     // TODO: 修改判断条件使测试通过
-    ASSERT(plus(0.1, 0.2) == 0.3, "How to make this pass?");
+    const double epsilon = 1e-9;  // 定义一个很小的误差范围
+    ASSERT(std::abs(plus(0.1, 0.2) - 0.3) < epsilon, "How to make this pass?");
 
     return 0;
 }
